be95ae062283284c2d70464bacd590e8a8b4a846
kcsheng <kacysheng@mail.com>
2021-06-20 14:58:18 +1000

    Update shopping list app by adding delete fucntion

--- shopping-list/assets/js/script.js
+++ shopping-list/assets/js/script.js
@@ -1,10 +1,15 @@
 const shoppingForm = $("#shopping-form");
 const shoppingList = $("#shopping-list");
-
 shoppingForm.on("submit", handleFormSubmit);
-
+$("ol").on("click", "li button", deleteItem);
 function handleFormSubmit(e) {
   e.preventDefault();
-  shoppingList.append(`<li>${$("#shopping-item").val()}</li>`);
+  shoppingList.append(
+    `<li>${$("#shopping-item").val()} <button>Delete</button></li>`
+  );
   $("#shopping-item").val("");
+}
+function deleteItem(e) {
+  let target = e.target;
+  $(target).parent().remove();
 }
